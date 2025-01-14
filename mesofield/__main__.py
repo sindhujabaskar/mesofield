ddd539a557fb8005a1eeed9f1aa3025bee368b83
Gronemeyer <130065262+sipelab@users.noreply.github.com>
2025-01-14 18:56:33 -0500

    Refactor: __main__ from click to argparse

--- mesofield/__main__.py
+++ mesofield/__main__.py
@@ -1,83 +1,72 @@
-"""Entry point for mesofield."""
+"""CLI Interface entry point for mesofield python package"""
 
 import os
-# os.environ['NUMEXPR_MAX_THREADS'] = '4'
-# os.environ['NUMEXPR_NUM_THREADS'] = '2'
-# import numexpr as ne 
-
-import click
+import argparse
 from PyQt6.QtWidgets import QApplication
 from mesofield.gui.maingui import MainWindow
 from mesofield.config import ExperimentConfig
 from mesofield.startup import HardwareManager
-'''
-This is the client terminal command line interface
 
-The client terminal commands are:
-
-    launch: Launch the mesofield acquisition interface
-        - dev: Set to True to launch in development mode with simulated MMCores
-    test_mda: Test the mesofield acquisition interface
-
-'''
-
-
-@click.group()
-def cli():
-    """mesofields Command Line Interface"""
-    pass
-
-@cli.command()
-@click.option('--dev', default=False, help='launch in development mode with simulated MMCores.')
-@click.option('--params', default='hardware.yaml', help='Path to the config JSON file.')
 def launch(dev, params):
-    """ Launch mesofield acquisition interface.
-    
-    This function initializes and launches the mesofield acquisition application. 
-    
-    It sets up the necessary hardware and configuration based on
-    the provided parameters.
-    
-    Parameters:
-    `dev (str)`: The device identifier to be used for the acquisition.
-    `params (str)`: The path to the configuration file. Default is the params.json file in the current directory.
-    
-    """
-    
+    """Launch the mesofield acquisition interface."""
     print('Launching mesofield acquisition interface...')
     app = QApplication([])
     config = ExperimentConfig(params, dev)
     config.hardware.configure_engines(config)
+    print(config.hardware.cameras[0])
     mesofield = MainWindow(config)
     mesofield.show()
-    app.exec_()
+    app.exec()
 
-@cli.command()
 def controller():
     """Launch the mesofield controller."""
     from mesofield.gui.widgets.controller import Controller
     app = QApplication([])
-    controller = Controller()
-    controller.show()
-    app.exec_()
+    c = Controller()
+    c.show()
+    app.exec()
 
-@cli.command()
-@click.option('--frames', default=100, help='Number of frames for the MDA test.')
 def test_mda(frames):
-    """
-    Run a test of the mesofield Multi-Dimensional Acquisition (MDA) 
-    """
+    """Run a test of the mesofield Multi-Dimensional Acquisition (MDA)."""
     from mesofield.startup import test_mda
 
-@cli.command()
-def run_mda():
+def run_mda_command():
     """Run the Multi-Dimensional Acquisition (MDA) without the GUI."""
-    run_mda()
 
+def main():
+    parser = argparse.ArgumentParser(description="mesofields Command Line Interface")
+    subparsers = parser.add_subparsers(dest='command', help='Available subcommands')
 
-if __name__ == "__main__":  # pragma: no cover
-    cli()
+    # Subcommand: launch
+    parser_launch = subparsers.add_parser('launch', help='Launch the mesofield acquisition interface')
+    parser_launch.add_argument('--dev', default=False, action='store_true',
+                               help='Launch in development mode with simulated MMCores')
+    parser_launch.add_argument('--params', default='hardware.yaml',
+                               help='Path to the config file')
 
+    # Subcommand: controller
+    parser_controller = subparsers.add_parser('controller', help='Launch the mesofield controller')
 
+    # Subcommand: test_mda
+    parser_test_mda = subparsers.add_parser('test_mda', help='Run a test MDA')
+    parser_test_mda.add_argument('--frames', type=int, default=100,
+                                 help='Number of frames for the MDA test')
 
+    # Subcommand: run_mda
+    subparsers.add_parser('run_mda', help='Run MDA without the GUI')
 
+    args = parser.parse_args()
+
+    if args.command == 'launch':
+        launch(args.dev, args.params)
+    elif args.command == 'controller':
+        controller()
+    elif args.command == 'test_mda':
+        test_mda(args.frames)
+    elif args.command == 'run_mda':
+        run_mda_command()
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
